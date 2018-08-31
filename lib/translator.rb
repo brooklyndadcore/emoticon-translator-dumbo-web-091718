dac4571b4fef2d7ccf9561159278e4a698ec6bad
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-31 02:26:19 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,24 @@
-# require modules here
+require 'pry'
+require 'yaml'
+# path = YAML.load_file('./lib/emoticons.yml')
 
-def load_library
-  # code goes here
+def load_library(emoticon_path)
+  path = YAML.load_file('./lib/emoticons.yml')
+  yammed_hash = {}
+  mean_arr = []
+  emot_arr = []
+  path.each do |meaning, emots|
+    mean_arr << {emots[1] => meaning}
+    emot_arr << {emots[0] => emots[1]} 
+  end
+  mean = mean_arr.reduce Hash.new, :merge
+  emot = emot_arr.reduce Hash.new, :merge
+  yammed_hash['get_meaning'] = mean
+  yammed_hash['get_emoticon'] = emot
+  return yammed_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path = YAML.load_file('./lib/emoticons.yml'), emoticon)
   # code goes here
 end
 
